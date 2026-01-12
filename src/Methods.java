4b1250ad1408473dd728b734f9a4f545976870db
ysaicr <sai8374473@gmail.com>
2026-01-12 19:37:55 +0530

    Fifth Script

---
+++ src/Methods.java
@@ -0,0 +1,23 @@
+   class Methods{
+    public static void main(String[] args) {
+        String name = "Keys";
+        System.out.println(name);
+        Addition(); /// method calling
+        subtraction();
+        multiplication();
+    }
+        public  static void Addition() ///  method defining
+        {
+            System.out.println("hi");
+        }
+        public static void subtraction()
+        {
+            System.out.println("hello");
+        }
+        public static void multiplication()
+        {
+            System.out.println("welcome");
+        }
+
+   }
+
